2e6ababc6df2fd6f651846f1fe77bfd93ae20d51
Djordje Djukic <djukicdjole@gmail.com>
2023-12-21 15:15:31 +0100

    Initial commit
    Initial commit

---
+++ src/main/java/com/logineko/TelemetryDataHub/TelemetryDataHubApplication.java
@@ -0,0 +1,13 @@
+package com.logineko.TelemetryDataHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryDataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryDataHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logineko/TelemetryDataHub/TelemetryDataHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logineko.TelemetryDataHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryDataHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
