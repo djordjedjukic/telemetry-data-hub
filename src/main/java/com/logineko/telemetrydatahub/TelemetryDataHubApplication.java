b5e184ef6d661d1b003ab9c2fe3f8606f6f96cc7
Djordje Djukic <djukicdjole@gmail.com>
2023-12-28 19:03:27 +0100

    create db automatically for easier testing

--- src/main/java/com/logineko/telemetrydatahub/TelemetryDataHubApplication.java
+++ src/main/java/com/logineko/telemetrydatahub/TelemetryDataHubApplication.java
@@ -1,12 +1,14 @@
 package com.logineko.telemetrydatahub;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TelemetryDataHubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TelemetryDataHubApplication.class, args);
 	}
--- src/main/java/com/logineko/telemetrydatahub/infrastructure/DocumentStoreHolder.java
+++ src/main/java/com/logineko/telemetrydatahub/infrastructure/DocumentStoreHolder.java
@@ -4,11 +4,19 @@ import com.logineko.telemetrydatahub.model.domain.Machine;
 import net.ravendb.client.documents.DocumentStore;
 import net.ravendb.client.documents.IDocumentStore;
 import net.ravendb.client.documents.conventions.DocumentConventions;
+import net.ravendb.client.serverwide.DatabaseRecord;
+import net.ravendb.client.serverwide.operations.CreateDatabaseOperation;
+import net.ravendb.client.serverwide.operations.GetDatabaseRecordOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DocumentStoreHolder {
+    @Bean
+    public static IDocumentStore getStore() {
+        return DocumentStoreContainer.store;
+    }
+
     private static class DocumentStoreContainer {
         public static final IDocumentStore store = new DocumentStore("http://localhost:8082", "LoginEko");
 
@@ -16,19 +24,25 @@ public class DocumentStoreHolder {
             DocumentConventions conventions = store.getConventions();
             conventions.setFindCollectionName(type -> {
 
-                if (Machine.class.isAssignableFrom(type))
-                    return "Machines";
+                if (Machine.class.isAssignableFrom(type)) return "Machines";
 
                 return DocumentConventions.defaultGetCollectionName(type);
             });
 
 
             store.initialize();
-        }
-    }
 
-    @Bean
-    public IDocumentStore getStore() {
-        return DocumentStoreContainer.store;
+            // Create database
+            // I wouldn't do this in real production code, this is just to be easier to test
+            try {
+                var operation = new GetDatabaseRecordOperation("LoginEko");
+                var databaseRecord = store.maintenance().server().send(operation);
+                if (databaseRecord == null) {
+                    store.maintenance().server().send(new CreateDatabaseOperation(new DatabaseRecord("LoginEko")));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
